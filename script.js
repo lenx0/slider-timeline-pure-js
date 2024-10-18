8ebbec26c2c891434ce08c5ebb49ec03c8cf54c8
thiago <thiagofreitas1106@gmail.com>
2024-10-18 12:41:56 -0300

    save

---
+++ script.js
@@ -0,0 +1,21 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    if (index >= totalSlides) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = totalSlides - 1;
+    }
+    const newTranslateValue = -slideIndex * 100;
+    document.querySelector('.slides').style.transform = `translateX(${newTranslateValue}%)`;
+}
+
+function moveSlide(step) {
+    slideIndex += step;
+    showSlide(slideIndex);
+}
+
+// Inicializar o primeiro slide
+showSlide(slideIndex);
