455abf26e481a64f10195d1766781fde51cf8e5b
thiago <thiagofreitas1106@gmail.com>
2024-10-18 15:29:20 -0300

    added more images and added timeline

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let slideIndex = 0;
 const slides = document.querySelectorAll('.slide-container');
 const totalSlides = slides.length;
+const years = document.querySelectorAll('.year');
+const separators = document.querySelectorAll('.separator');
 
 function showSlide(index) {
     if (index >= totalSlides) {
@@ -10,6 +12,30 @@ function showSlide(index) {
     }
     const newTranslateValue = -slideIndex * 100;
     document.querySelector('.slides').style.transform = `translateX(${newTranslateValue}%)`;
+    
+    // Atualiza a timeline
+    updateTimeline();
+}
+
+function updateTimeline() {
+    years.forEach((year, index) => {
+        if (index === slideIndex) {
+            year.classList.remove('gray-text');
+            year.classList.add('green-text');
+            separators[index].classList.remove('gray-text');
+            separators[index].classList.add('green-text');
+        } else if (index < slideIndex) {
+            year.classList.remove('gray-text');
+            year.classList.add('green-text');
+            separators[index].classList.remove('gray-text');
+            separators[index].classList.add('green-text');
+        } else {
+            year.classList.remove('green-text');
+            year.classList.add('gray-text');
+            separators[index].classList.remove('green-text');
+            separators[index].classList.add('gray-text');
+        }
+    });
 }
 
 function moveSlide(step) {
